52ac4b065ccec87eda1889604c4a892c2cf71056
John Muchiri <muchirijohn0018@gmail.com>
2026-01-10 14:17:51 +0300

    update dependencies to support options

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,9 +1,16 @@
+
+import { Pool } from "pg";
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from "../generated/prisma/client";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from 'better-auth/adapters/prisma'
 import { nextCookies } from 'better-auth/next-js'
 
-const prisma = new PrismaClient()
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({ connectionString });
+
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({adapter});
 export const auth = betterAuth({
   database: prismaAdapter(prisma,
     {
